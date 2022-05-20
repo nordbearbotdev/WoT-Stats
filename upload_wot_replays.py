00de1259ba6b767bb66a7c092619e0a7b4570bb7
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-05-20 22:54:56 +0300

    Update upload_wot_replays.py

--- upload_wot_replays.py
+++ upload_wot_replays.py
@@ -17,3 +17,18 @@ import re, concurrent.futures, configparser, motor.motor_asyncio, ssl, zipfile
 import blitzutils as bu
 from blitzutils import WG
 from blitzutils import WoTinspector
+
+logging.getLogger("asyncio").setLevel(logging.DEBUG)
+FILE_CONFIG 	= 
+
+DEBUG 	= False
+VERBOSE = False
+SLEEP 		= 2
+MAX_RETRIES = 3
+REPLAY_N 	= 0
+SKIPPED_N 	= 0
+ERROR_N 	= 0
+WIurl='https://wotinspector.com/api/replay/upload?'
+WG_appID  = 'c688c996873daa7657609b096c46dfe7'
+wg = None
+wi = None
